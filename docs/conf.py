12c2d41f6895d450d667dfab9deda31153838b8f
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-05-27 16:18:41 -0400

    throw a nice importerror if sphinx_rtd_theme not installed

--- docs/conf.py
+++ docs/conf.py
@@ -163,7 +163,13 @@ import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+    try:
+        import sphinx_rtd_theme
+    except ImportError:
+        raise ImportError("It looks like you don't have the sphinx_rtd_theme "
+                          "package installed. The astroplan documentation "
+                          "uses Read The Docs, so you must install this first."
+                          "For example, pip install sphinx_rtd_theme")
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
