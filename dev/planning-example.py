08e5442afec5831fe3a97cbba6dd5359fdbde769
Brett Morris <brettmorris21@gmail.com>
2015-06-19 15:21:36 -0700

    Maybe last iteration before merge?

--- dev/planning-example.py
+++ dev/planning-example.py
@@ -41,48 +41,23 @@ at that location.
 # Accept `timezone` from pytz
 from astroplan import Observer
 import astropy.units as u
+from astropy.coordinates import EarthLocation
 import pytz
 
-obs = Observer(name='Subaru Telescope',
-               longitude='-155:28:48.900',
-               latitude='+19:49:42.600',
-               elevation=4163 * u.meter,
-               pressure=0.615 * u.bar,
-               relative_humidity=0.11,
-               temperature=0 * u.deg_C,
-               timezone=pytz.timezone('US/Hawaii'),
-               description="Subaru Telescope on Mauna Kea, Hawaii")
-
-# Define the same observer with an instance of astropy.coordinates.EarthLocation,
+# Define the observer with an instance of astropy.coordinates.EarthLocation,
 # also use pytz.timezone() argument directly as `timezone` keyword's input
-from astropy.coordinates import EarthLocation
-obs = Observer(name='Subaru',
-               location=EarthLocation(lon='-155:28:48.900',
-                                      lat='+19:49:42.600'),
-               elevation=4163 * u.meter,
+longitude = '-155d28m48.900s'
+latitude = '+19d49m42.600s'
+elevation = 4163 * u.m
+location = EarthLocation.from_geodetic(longitude, latitude, elevation)
+
+obs = Observer(name='Subaru Telescope',
+               location=location,
                pressure=0.615 * u.bar,
                relative_humidity=0.11,
                temperature=0 * u.deg_C,
-               timezone='US/Hawaii',
-               description="Subaru Telescope on Mauna Kea, Hawaii")
-
-# longitude and latitude would be represented internally by astropy
-# coordinates.angles.Longitude/Latitude.  So any value that can naturally 
-# initialize one of those objects could be passed as the parameter to Observer
-
-# It's also possible to initialize using astropy objects and other
-# direct entities
-from astropy.coordinates import Longitude, Latitude
-
-obs = Observer(name='Keck 1',
-               longitude=Longitude(155.4750 * u.degree),
-               latitude=Latitude(19.8264 * u.degree),
-               elevation=4160 * u.m,
-               pressure=0.615 * u.bar,
-               relative_humidity=0.11,
-               temperature=0.0 * u.deg_C,
                timezone=pytz.timezone('US/Hawaii'),
-               description="W.M. Keck 1 Telescope on Mauna Kea, Hawaii")
+               description="Subaru Telescope on Mauna Kea, Hawaii")
 
 # It would also be desirable to be able to have a small database of
 # common telescopes.  Maybe this can at first simply take the form of
@@ -91,11 +66,10 @@ from astroplan import sites
 
 obs = sites.Keck1
 
-# Environmental conditions should be updatable.  Suggest using keyword
-# parameters (this would make it easy to update an internal AltAz coordinate):
-
-obs.set_environment(pressure=0.600 * u.bar, relative_humidity=0.2,
-                    temperature=10 * u.deg_C)
+# Environmental conditions should be updatable.
+obs.pressure = 0.600 * u.bar
+obs.relative_humidity = 0.2
+obs.temperature = 10 * u.deg_C
 
 # Dates are assumed to be UTC by default
 
@@ -111,55 +85,56 @@ from astropy.time import Time
 time_obs = Time(2457189.500000, format='jd')
 
 # sunset
-obs.sunset(date=time_obs, which='nearest') # Default
-obs.sunset(date=time_obs, which='next')
-obs.sunset(date=time_obs, which='previous')
+obs.sunset(time=time_obs, which='nearest') # Default
+obs.sunset(time=time_obs, which='next')
+obs.sunset(time=time_obs, which='previous')
+obs.sunset() # Will be use time=Time.now(), which='nearest'
 
 # sunrise
-obs.sunrise(date=time_obs, which='nearest') # Default
-obs.sunrise(date=time_obs, which='next')
-obs.sunrise(date=time_obs, which='previous')
+obs.sunrise(time=time_obs, which='nearest') # Default
+obs.sunrise(time=time_obs, which='next')
+obs.sunrise(time=time_obs, which='previous')
 
 # moon rise
-obs.moon_rise(date=time_obs, which='nearest')
+obs.moonrise(time=time_obs, which='nearest')
 
 # moon set
-obs.moon_set(date=time_obs, which='nearest')
+obs.moonset(time=time_obs, which='nearest')
 
 # The above functions can be called with an `angle` keyword argument to specify
 # a particular horizon angle for rising or setting, or can be called with
 # convenience functions for particular morning/evening twilight.
 # For example, to compute astronomical twilight by specifying the `angle`:
-obs.sunset(date=time_obs, which='next', angle=18*u.degree) 
+obs.sunset(time=time_obs, which='next', angle=18*u.degree) 
 
 # evening (astronomical) twilight
-obs.evening_astronomical(date=time_obs)
+obs.evening_astronomical(time=time_obs)
 
 # evening (nautical) twilight
-obs.evening_nautical(date=time_obs)
+obs.evening_nautical(time=time_obs)
 
 # evening (civil) twilight
-obs.evening_civil(date=time_obs)
+obs.evening_civil(time=time_obs)
 
 # morning (nautical) twilight
-obs.morning_nautical(date=time_obs)
+obs.morning_nautical(time=time_obs)
 
 # morning (civil) twilight
-obs.morning_civil(date=time_obs)
+obs.morning_civil(time=time_obs)
 
 # morning (astronomical) twilight
-obs.morning_astronomical(date=time_obs)
+obs.morning_astronomical(time=time_obs)
 
 # what is the moon illumination?
 # returns a float, which is percentage of the moon illuminated
-obs.moon_illumination(date=time_obs)
+obs.moon_illumination(time=time_obs)
 
 # what is the moon altitude and azimuth?
-obs.moon_altaz(date=time_obs)
+obs.moon_altaz(time=time_obs)
 
 # Other sun-related convenience functions:
-obs.noon(date=time_obs, which='nearest')
-obs.midnight(date=time_obs, which='nearest')
+obs.noon(time=time_obs, which='nearest')
+obs.midnight(time=time_obs, which='nearest')
 
 # ==============
 # Target objects
@@ -171,13 +146,10 @@ and an optional name.
 from astroplan import FixedTarget, airmass_plot
 
 # Define a target.
-t1 = FixedTarget(name='Polaris', ra='02:31:49.09', dec='+89:15:50.8')
 
-# initializing from astropy entities directly
 from astropy.coordinates import SkyCoord
-
 t1 = FixedTarget(name='Polaris',
-               coord=SkyCoord('02h31m49.09s', '+89d15m50.8s', frame='icrs'))
+                 coord=SkyCoord('02h31m49.09s', '+89d15m50.8s', frame='icrs'))
 
 # Leaves scope for NonSiderealTarget, etc.
 
@@ -200,19 +172,17 @@ from astroplan import TimeWindow, AltitudeWindow, AboveAirmass, is_observable
 
 # times can be passed in as strings (interpreted as for get_date()) or
 # as astropy Time or datetime objects
-constraint_list = [TimeWindow("2015-05-01 18:30", "2015-05-02 05:30"),
-                   AltitudeWindow(15.0*u.deg, 89.0*u.deg), AboveAirmass(1.2)]
+time1, time2 = Time("2015-05-01 18:30"), Time("2015-05-02 05:30")
+constraint_list = [TimeWindow(time1, time2), AboveAirmass(1.2),
+                   AltitudeWindow(15.0*u.deg, 89.0*u.deg)]
 # (AboveAirmass will be a subclass of AltitudeWindow)
-
-# Define a target
-tgt = FixedTarget(name='S5', ra='14:20:00.00', dec='48:00:00.00')
 
 # Combine a list of constraints to run on Observer, FixedTarget, and time to 
 # determine the observability of target
-constraints = is_observable(constraint_list, obs, tgt, time_obs)
+constraints = is_observable(constraint_list, obs, t1, time_obs)
 
 # Test only a single constraint:
-constraints = is_observable(AboveAirmass(1.2), obs, tgt, time_obs)
+constraints = is_observable(AboveAirmass(1.2), obs, t1, time_obs)
 
 # `constraints` will be a boolean where True=observable. For a list of 
 # targets, observatories, or times, `constraints` may be a booleans array
@@ -227,8 +197,8 @@ constraints = is_observable(AboveAirmass(1.2), obs, tgt, time_obs)
 
 # calculate the distance in alt and az degrees between two targets at
 # the given time (e.g. to calculate slew time)
-sf = FixedTarget(name='Sf', ra='09:40:00.00', dec='43:00:00.00')
-sm = FixedTarget(name='Sm', ra='10:30:00.00', dec='36:00:00.00')
+sf = FixedTarget(name='Sf', coord=SkyCoord('09d40m00.00s', '43d00m00.00s'))
+sm = FixedTarget(name='Sm', coord=SkyCoord('10d30m00.00s', '36d00m00.00s'))
 
 # Coordinate arithmetic gives separations in RA, Dec, alt, az
 dra, ddec = sf.ra - sm.ra, sf.dec - sm.dec
