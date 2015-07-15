d576ee98821fb57093f0cde73f269bdf04eeebbb
Brett Morris <brettmorris21@gmail.com>
2015-07-15 10:14:08 -0700

    Putting tz before name

--- astroplan/core.py
+++ astroplan/core.py
@@ -81,7 +81,7 @@ class Observer(object):
     TODO: write this docstring
     """
     @u.quantity_input(elevation=u.m)
-    def __init__(self, location=None, name=None, timezone='UTC', latitude=None,
+    def __init__(self, location=None, timezone='UTC', name=None, latitude=None,
                  longitude=None, elevation=0*u.m, pressure=None,
                  relative_humidity=None, temperature=None, description=None):
         """
