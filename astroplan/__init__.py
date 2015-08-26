358224acee833d19c76f1e1b08bbda51caa52d62
Brett Morris <brettmorris21@gmail.com>
2015-08-25 18:57:24 -0700

    Well caching is messy, huh.

--- astroplan/__init__.py
+++ astroplan/__init__.py
@@ -13,8 +13,9 @@ from ._astropy_init import *
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
     from .utils import *
-
     from .core import *
     from .exceptions import *
     from .sites import *
     from .moon import *
+
+    get_IERS_A_or_workaround()
--- astroplan/utils.py
+++ astroplan/utils.py
@@ -1,10 +1,118 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+import warnings
+import numpy as np
+from astropy.utils.data import download_file, clear_download_cache
+from astropy.utils import iers
+from astropy.time import Time
+import astropy.units as u
+from .exceptions import OldEarthOrientationDataWarning
+from astropy.utils.data import (_get_download_cache_locs, CacheMissingWarning,
+                                _open_shelve)
 
-__all__ = ["_mock_remote_data"]
+__all__ = ["_mock_remote_data", "get_IERS_A_or_workaround", "download_IERS_A"]
+
+IERS_A_WARNING = ("For best precision (on the order of arcseconds), you must "
+                  "download an up-to-date IERS Bulletin A table. To do so, run:"
+                  "\n\n"
+                  ">>> from astroplan import download_IERS_A\n"
+                  ">>> download_IERS_A()\n")
+
+BACKUP_Time_get_delta_ut1_utc = Time._get_delta_ut1_utc
 
 def _mock_remote_data():
     # Mock FixedTarget.from_name class method for tests without remote data
     from astroplan.core import FixedTarget
     FixedTarget.from_name = FixedTarget._fixed_target_from_name_mock
+
+def _low_precision_utc_to_ut1(self, jd1, jd2):
+    """
+    When no IERS Bulletin A is available (no internet connection), use low
+    precision time conversion by assuming UT1-UTC=0 always.
+    This method mimics `~astropy.coordinates.builtin_frames.utils.get_dut1utc`
+    """
+    try:
+        return self.delta_ut1_utc
+    except IndexError:
+        warnings.warn(IERS_A_WARNING, OldEarthOrientationDataWarning)
+        return np.zeros(self.shape)
+
+def get_IERS_A_or_workaround():
+    """
+    Get the cached IERS Bulletin A table if one exists. If one does not exist,
+    monkey patch `~astropy.time.Time._get_delta_ut1_utc` so that
+    `~astropy.time.Time` objects don't raise errors by computing UT1-UTC off
+    the end of the IERS table.
+    """
+    if IERS_A_in_cache():
+        iers.IERS.iers_table = _get_IERS_A_table()
+    else:
+        Time._get_delta_ut1_utc = _low_precision_utc_to_ut1
+
+def IERS_A_in_cache():
+    """
+    Check if the IERS Bulletin A table is locally cached.
+    """
+    url_key = iers.IERS_A_URL
+    # The below code which accesses ``urlmapfn`` is stolen from
+    # astropy.utils.data.download_file()
+    try:
+        dldir, urlmapfn = _get_download_cache_locs()
+    except (IOError, OSError) as e:
+        msg = 'Remote data cache could not be accessed due to '
+        estr = '' if len(e.args) < 1 else (': ' + str(e))
+        warnings.warn(CacheMissingWarning(msg + e.__class__.__name__ + estr))
+        return False
+    with _open_shelve(urlmapfn, True) as url2hash:
+        if url_key in url2hash:
+            return True
+    return False
+
+def _get_IERS_A_table(warn_update=7*u.day):
+    """
+    Grab the locally cached copy of the IERS Bulletin A table. Check to see
+    if it's up to date, and warn the user if it is not.
+
+    This will fail and raise OSError if the file is not in the cache.
+    """
+    if IERS_A_in_cache():
+        path = download_file(iers.IERS_A_URL, cache=True, show_progress=True)
+        table = iers.IERS_A.open(path)
+        # Use polar motion flag to identify last observation before predictions
+        index_of_last_observation = ''.join(table['PolPMFlag_A']).index('IP')
+        time_of_last_observation = Time(table['MJD'][index_of_last_observation],
+                                        format='mjd')
+        time_since_last_update = Time.now() - time_of_last_observation
+
+        # If the IERS bulletin is more than 7 days old, warn user
+        if warn_update < time_since_last_update:
+            warnmsg = ("Your version of the IERS Bulletin A is {:.1f} days "
+                       "old. ".format(time_since_last_update.to(u.day).value) +
+                       IERS_A_WARNING)
+            warnings.warn(warnmsg, OldEarthOrientationDataWarning)
+        return table
+    else:
+        raise OSError("No IERS A table has been downloaded.")
+
+def download_IERS_A(show_progress=True):
+    """
+    Download and cache the IERS Bulletin A table.
+
+    If one is already cached, download a new one and overwrite the old. Store
+    table in the astropy cache, and undo the monkey patching done by
+    `~astroplan.utils.get_IERS_A_or_workaround`.
+
+    Parameters
+    ----------
+    show_progress : bool
+        `True` shows a progress bar during the download.
+    """
+    if IERS_A_in_cache():
+        clear_download_cache(iers.IERS_A_URL)
+
+    local_iers_a_path = download_file(iers.IERS_A_URL, cache=True,
+                                      show_progress=show_progress)
+    # Undo monkey patch set up by get_IERS_A_or_workaround
+    iers.IERS.iers_table = local_iers_a_path
+    Time._get_delta_ut1_utc = BACKUP_Time_get_delta_ut1_utc
