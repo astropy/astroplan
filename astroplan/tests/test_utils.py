632ae58a63488e4aef6f9a0ce92de13df2401fd9
Erik Tollerud <erik.tollerud@gmail.com>
2015-08-27 17:10:33 -0400

    fix test to work even if IERS table already present

--- astroplan/tests/test_utils.py
+++ astroplan/tests/test_utils.py
@@ -8,17 +8,23 @@ from astropy.tests.helper import remote_data
 from astropy.utils.data import clear_download_cache
 from astropy.utils import iers
 
-from ..utils import download_IERS_A, IERS_A_in_cache
+from ..utils import (download_IERS_A, IERS_A_in_cache, get_IERS_A_or_workaround,
+                     BACKUP_Time_get_delta_ut1_utc)
 from ..exceptions import OldEarthOrientationDataWarning
 
 @remote_data
 def test_iers_download(monkeypatch, recwarn):
     # the monkeypatch is here to undo the changes that importing astroplan does
     # if the IERS A tables already exist
-    monkeypatch.setattr(iers.IERS_A, 'iers_table', None)
-
     if IERS_A_in_cache():
         clear_download_cache(iers.IERS_A_URL)
+
+    monkeypatch.setattr(iers.IERS, 'iers_table', None)
+    monkeypatch.setattr(iers.IERS_A, 'iers_table', None)
+    monkeypatch.setattr(Time, '_get_delta_ut1_utc', BACKUP_Time_get_delta_ut1_utc)
+
+    # now make sure the state is what it should be given the above changes
+    get_IERS_A_or_workaround()
 
     # first make sure a future time gives a warning with IERS A missing
     nowplusoneyear = Time.now() + 1*u.year
