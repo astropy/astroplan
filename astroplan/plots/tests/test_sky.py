8ef82e272f1829012a26d92a22cc181ec925a152
Christoph Deil <Deil.Christoph@gmail.com>
2015-08-28 15:55:04 +0200

    Install matplotlib on travis-ci

--- astroplan/plots/tests/test_sky.py
+++ astroplan/plots/tests/test_sky.py
@@ -2,15 +2,22 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import pytest
-import matplotlib.pyplot as plt
+
+try:
+    import matplotlib
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
 
 
 # TODO: replace this with actual plot checks once these
 # issues are resolved:
 # https://github.com/astropy/astroplan/issues/65
 # https://github.com/astropy/astroplan/issues/74
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
 @pytest.mark.mpl_image_compare
 def test_image_example():
+    import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.plot([1, 2, 3])
