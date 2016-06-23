df8a1446403e487f87bf26769a4b35487068714d
kvyh <karlvyh@hotmail.com>
2016-06-23 11:01:27 -0700

    more tests

--- astroplan/tests/test_scheduling.py
+++ astroplan/tests/test_scheduling.py
@@ -2,14 +2,16 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import numpy as np
 from astropy.time import Time
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 
 from ..observer import Observer
 from ..target import FixedTarget
-from ..constraints import (AirmassConstraint)
-from ..scheduling import (ObservingBlock, PriorityScheduler, SequentialScheduler, Transitioner)
+from ..constraints import (AirmassConstraint, _get_altaz)
+from ..scheduling import (ObservingBlock, PriorityScheduler, SequentialScheduler,
+                          Transitioner, TransitionBlock)
 
 vega = FixedTarget(coord=SkyCoord(ra=279.23473479 * u.deg, dec=38.78368896 * u.deg),
                    name="Vega")
@@ -30,7 +32,9 @@ def test_priority_scheduler():
     scheduler = PriorityScheduler(start_time, end_time,
                                   constraints=constraints, observer=mdm)
     schedule = scheduler(blocks)
-    assert len(schedule.observing_blocks) > 0
+    assert len(schedule.observing_blocks) == 3
+    assert all(np.abs(block.end_time - block.start_time - block.duration) <
+               1*u.second for block in schedule.scheduled_blocks)
 
 
 def test_sequential_scheduler():
@@ -44,3 +48,18 @@ def test_sequential_scheduler():
                                     transitioner=trans)
     schedule = scheduler(blocks)
     assert len(schedule.observing_blocks) > 0
+    assert all(np.abs(block.end_time - block.start_time - block.duration) <
+               1*u.second for block in schedule.scheduled_blocks)
+
+
+def test_transitioner():
+    blocks = [ObservingBlock(t, 55 * u.minute, i) for i, t in enumerate(targets)]
+    slew_rate = 1 * u.deg / u.second
+    transitioner = Transitioner(slew_rate=slew_rate)
+    start_time = Time('2016-02-06 00:00:00')
+    trans = transitioner(blocks[0], blocks[1], start_time, mdm)
+    aaz = _get_altaz(Time([start_time]), mdm,
+                     [blocks[0].target, blocks[1].target])['altaz']
+    sep = aaz[0].separation(aaz[1])[0]
+    assert isinstance(trans, TransitionBlock)
+    assert trans.duration == sep/slew_rate
