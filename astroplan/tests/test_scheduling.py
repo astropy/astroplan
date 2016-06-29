9b9825691cf4c926b58b119b68498299f710e9a6
kvyh <karlvyh@hotmail.com>
2016-06-29 16:01:17 -0700

    added tests for Slot and Schedule classes

--- astroplan/tests/test_scheduling.py
+++ astroplan/tests/test_scheduling.py
@@ -11,7 +11,7 @@ from ..observer import Observer
 from ..target import FixedTarget
 from ..constraints import (AirmassConstraint, _get_altaz)
 from ..scheduling import (ObservingBlock, PriorityScheduler, SequentialScheduler,
-                          Transitioner, TransitionBlock)
+                          Transitioner, TransitionBlock, Schedule, Slot)
 
 vega = FixedTarget(coord=SkyCoord(ra=279.23473479 * u.deg, dec=38.78368896 * u.deg),
                    name="Vega")
@@ -24,12 +24,26 @@ mdm = Observer.at_site('mdm')
 targets = [vega, rigel, polaris]
 
 
+def test_transitioner():
+    blocks = [ObservingBlock(t, 55 * u.minute, i) for i, t in enumerate(targets)]
+    slew_rate = 1 * u.deg / u.second
+    trans = Transitioner(slew_rate=slew_rate)
+    start_time = Time('2016-02-06 00:00:00')
+    transition = trans(blocks[0], blocks[1], start_time, mdm)
+    aaz = _get_altaz(Time([start_time]), mdm,
+                     [blocks[0].target, blocks[1].target])['altaz']
+    sep = aaz[0].separation(aaz[1])[0]
+    assert isinstance(transition, TransitionBlock)
+    assert transition.duration == sep/slew_rate
+transitioner = Transitioner(slew_rate=1 * u.deg / u.second)
+
+
 def test_priority_scheduler():
     constraints = [AirmassConstraint(3, boolean_constraint=False)]
     blocks = [ObservingBlock(t, 55*u.minute, i) for i, t in enumerate(targets)]
     start_time = Time('2016-02-06 00:00:00')
     end_time = start_time + 24*u.hour
-    scheduler = PriorityScheduler(start_time, end_time,
+    scheduler = PriorityScheduler(start_time, end_time, transitioner=transitioner,
                                   constraints=constraints, observer=mdm)
     schedule = scheduler(blocks)
     assert len(schedule.observing_blocks) == 3
@@ -42,24 +56,35 @@ def test_sequential_scheduler():
     blocks = [ObservingBlock(t, 55 * u.minute, i) for i, t in enumerate(targets)]
     start_time = Time('2016-02-06 00:00:00')
     end_time = start_time + 24 * u.hour
-    trans = Transitioner(slew_rate=1*u.deg/u.second)
     scheduler = SequentialScheduler(start_time, end_time,
                                     constraints=constraints, observer=mdm,
-                                    transitioner=trans)
+                                    transitioner=transitioner)
     schedule = scheduler(blocks)
     assert len(schedule.observing_blocks) > 0
     assert all(np.abs(block.end_time - block.start_time - block.duration) <
                1*u.second for block in schedule.scheduled_blocks)
 
 
-def test_transitioner():
-    blocks = [ObservingBlock(t, 55 * u.minute, i) for i, t in enumerate(targets)]
-    slew_rate = 1 * u.deg / u.second
-    transitioner = Transitioner(slew_rate=slew_rate)
+def test_slot():
     start_time = Time('2016-02-06 00:00:00')
-    trans = transitioner(blocks[0], blocks[1], start_time, mdm)
-    aaz = _get_altaz(Time([start_time]), mdm,
-                     [blocks[0].target, blocks[1].target])['altaz']
-    sep = aaz[0].separation(aaz[1])[0]
-    assert isinstance(trans, TransitionBlock)
-    assert trans.duration == sep/slew_rate
+    end_time = start_time + 24 * u.hour
+    slot = Slot(start_time, end_time)
+    slots = slot.split_slot(start_time, start_time+1*u.hour)
+    assert len(slots) == 2
+    assert slots[0].end == slots[1].start
+
+
+def test_schedule():
+    start_time = Time('2016-02-06 00:00:00')
+    end_time = start_time + 24 * u.hour
+    schedule = Schedule(start_time, end_time)
+    assert schedule.slots[0].start == start_time
+    assert schedule.slots[0].end == end_time
+    assert schedule.slots[0].duration == 24*u.hour
+    schedule.new_slots(0, start_time, end_time)
+    assert len(schedule.slots) == 1
+    new_slots = schedule.new_slots(0, start_time+1*u.hour, start_time+4*u.hour)
+    assert np.abs(new_slots[0].duration - 1*u.hour) < 1*u.second
+    assert np.abs(new_slots[1].duration - 3*u.hour) < 1*u.second
+    assert np.abs(new_slots[2].duration - 20*u.hour) < 1*u.second
+
