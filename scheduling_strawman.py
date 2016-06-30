4abe7592a0338313a26178f1f3a7c7821b83348f
kvyh <karlvyh@hotmail.com>
2016-06-30 14:48:35 -0700

    changed some of the formatting and included suggestions

--- scheduling_strawman.py
+++ scheduling_strawman.py
@@ -21,6 +21,13 @@ schedule_end = schedule_start+24*u.hour
 # define the observer
 mro = Observer.at_site('mro')
 
+# we also want to define how quickly the telescope can transition
+# between different targets, filters, instruments etc.
+# for using a telescope that slews at 1 degree/second,
+# takes 2 minutes to change filters, and 10 to change instruments
+slew_rate = 1*u.deg/u*second
+transitioner = Transitioner(slew_rate, {filter: 2*u.minute, instrument: 10*u.minute})
+
 # make the targets (this can also use FixedTarget(coords) if you make a
 #                   `SkyCoord` object "coords")
 targets = [FixedTarget.from_name('vega'),
@@ -40,38 +47,36 @@ priorities = {target.name: i for i, target in enumerate(targets)}
 visit_duration = 55*u.minute
 blocks = []
 
-# an `ObservingBlock` contains the observation of one target for a duartion.
+# a `BlockGroup` contains one or more `ObservingBlocks` that are related
 
-# create a block for each target with the constraint that the airmass be less than 3
+# create blocks for the targets  with the constraint that the airmass be less than 3
 # for that target during its OB and a non-boolean score (since constraints default to a boolean)
-# so that lower airmass gets a higher score.
+# so that lower airmass gets a higher score. We also want the moon to be farther than 60 degrees
+# away, but lower airmass is better than the moon being farther, so we give them weights.
 for target in targets:
-    OB = ObservingBlock(target, visit_duration, priority=priorities[target.name],
-                        constraints=[AirmassConstrant(max=3, boolean_score=False)]
-                        )
-    blocks.append(OB)
-
-# a `BlockGroup` contains multiple `ObservingBlocks` that are related
+    block = BlockGroup(target, visit_duration, priority=priorities[target.name],
+                       constraints=[AirmassConstraint(max=3, boolean_score=False),
+                                    MoonSeparationConstraint(60*u.deg, boolean_score=False)],
+                       constraint_weights=[1, .2],
+                       instrument_configurations=[{filter: 'v'}]
+                       )
+    blocks.append(block)
 
 # create a `BlockGroup` to make sure Alcor and M101 are observed consecutively
 # we don't care how good the airmass is for them, just that it is below 3 and
 # that the observations are done during grey time.
 # we also want the group prioritized after the single targets and
 # to observe M101 immediately before Alcor (i.e. order = [1, 0]
-blocks.append(BlockGroup(target_group, durations=[54*u.minute for target in target_group],
+blocks.append(BlockGroup(target_group, [visit_duration for target in target_group],
                          priority=max(priorities)+1, order=[1, 0],
-                         constraints=[AirmassConstraint(3), MoonIlluminationConstraint.grey()]
+                         constraints=[AirmassConstraint(3), MoonIlluminationConstraint.grey()],
+                         instrument_configurations=[{filter: 'b'}, {filter: 'v'}],
+                         transitioner = transitioner
                          ))
 
 # The observatory is optical so all ObservationBlocks need to be scheduled at night
 # it also can only point between 10 and 80 degrees altitude.
-observer_constraints = [AtNightconstraint(), AltitudeConstraint(10*u.deg, 80*u.deg)]
-
-# The final piece we need to define, is how quickly the telescope can transition
-# between different objects
-# for using a telescope that slews at 1 degree/second and takes 30 seconds to change filters
-slew_rate = 1*u.deg/u*second
-transitioner = Transitioner(slew_rate, filter_time=30*u.second)
+observer_constraints = [AtNightConstraint(), AltitudeConstraint(10*u.deg, 80*u.deg)]
 
 # now create a scheduler object, in this case we want the scheduler which uses
 # the Greedy method for scheduling
@@ -87,7 +92,7 @@ print(schedule.scheduled)
 # to see the above, but in table form instead (with columns of target, start, end, target_ra, target_dec, score, etc.)
 print(schedule.to_table)
 
-# to see the entire schedule including transitions and open slots
+#
 print(schedule)
 
 # to see how much of the time is spent observing
