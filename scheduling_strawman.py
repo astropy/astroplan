011e14f4ab9922f1e32040d083d291bc27614ae9
kvyh <karlvyh@hotmail.com>
2016-07-07 15:35:04 -0700

    going back to ObservingBlocks and some variable name changes

--- scheduling_strawman.py
+++ scheduling_strawman.py
@@ -1,7 +1,7 @@
 """
 The scheduler is a very useful tool. It requires the input of observing blocks
 and the time_range. Observing blocks consist of a target, a duration and constraints.
-"global" constraints can also be defined and can significantly speed up the scheduler.
+"global" constraints can also be defined that apply to every observing block.
 """
 
 # a few packages are needed to set up the proper objects
@@ -14,19 +14,22 @@ from astroplan.constraints import *
 from astroplan import Observer, FixedTarget
 from astroplan.scheduling import *
 
-# create the time frame during which you observing
-schedule_start = Time.now()+4*u.hour
-schedule_end = schedule_start+24*u.hour
+# create the time frame during which you are observing
+schedule_start = Time.now() + 4*u.hour
+schedule_end = schedule_start + 24*u.hour
 
 # define the observer
-mro = Observer.at_site('mro')
+apo = Observer.at_site('apo')
 
 # we also want to define how quickly the telescope can transition
 # between different targets, filters, instruments etc.
 # for using a telescope that slews at 1 degree/second,
 # takes 2 minutes to change filters, and 10 to change instruments
 slew_rate = 1*u.deg/u*second
-transitioner = Transitioner(slew_rate, {filter: 2*u.minute, instrument: 10*u.minute})
+filter_times = {'default': 2*u.minute, ('v','I'): 3*u.minute}
+instrument_times = {'devault': 10*u.minute, ('SPICAM', 'DIS'): 7*u.minute}
+transitioner = Transitioner(slew_rate, {'filter': filter_times,
+                                        'instrument': instrument_times})
 
 # make the targets (this can also use FixedTarget(coords) if you make a
 #                   `SkyCoord` object "coords")
@@ -44,7 +47,8 @@ target_group = [FixedTarget.from_name('Alcor'),
 priorities = {target.name: i for i, target in enumerate(targets)}
 
 # now `ObservingBlock`s and `BlockGroup`s need to be defined
-visit_duration = 55*u.minute
+# define the duration of the ObservingBlocks
+block_duration = 55*u.minute
 blocks = []
 
 # a `BlockGroup` contains one or more `ObservingBlocks` that are related
@@ -54,11 +58,11 @@ blocks = []
 # so that lower airmass gets a higher score. We also want the moon to be farther than 60 degrees
 # away, but lower airmass is better than the moon being farther, so we give them weights.
 for target in targets:
-    block = BlockGroup(target, visit_duration, priority=priorities[target.name],
+    block = ObservingBlock(target, block_duration, priority=priorities[target.name],
                        constraints=[AirmassConstraint(max=3, boolean_score=False),
                                     MoonSeparationConstraint(60*u.deg, boolean_score=False)],
                        constraint_weights=[1, .2],
-                       instrument_configurations=[{filter: 'v'}]
+                       instrument_configuration=[{filter: 'v'}]
                        )
     blocks.append(block)
 
@@ -66,22 +70,30 @@ for target in targets:
 # we don't care how good the airmass is for them, just that it is below 3 and
 # that the observations are done during grey time.
 # we also want the group prioritized after the single targets and
-# to observe M101 immediately before Alcor (i.e. order = [1, 0]
-blocks.append(BlockGroup(target_group, [visit_duration for target in target_group],
-                         priority=max(priorities)+1, order=[1, 0],
-                         constraints=[AirmassConstraint(3), MoonIlluminationConstraint.grey()],
-                         instrument_configurations=[{filter: 'b'}, {filter: 'v'}],
-                         transitioner = transitioner
-                         ))
+# we want M101 observed before Alcor, so we need to change the order
+instrument_configurations = [{filter: 'b'}, {filter: 'v'}]
+block_durations = [40*u.minute, 90*u.minute]
+grouped_blocks = []
+for i, target in target_group:
+    block = ObservingBlock(target, block_durations[i],
+                           constraints=[AirmassConstraint(3),
+                                        MoonIlluminationConstraint.grey()],
+                           constraint_weights=[1, .2],
+                           instrument_configurations=instrument_configurations[i],
+                           )
+    grouped_blocks.append(block)
+block_group = BlockGroup(grouped_blocks,  schedule_constraint='consecutive',
+                         order=[1, 0], priority=max(priorities) + 1)
+blocks.append(block_group)
 
 # The observatory is optical so all ObservationBlocks need to be scheduled at night
 # it also can only point between 10 and 80 degrees altitude.
-observer_constraints = [AtNightConstraint(), AltitudeConstraint(10*u.deg, 80*u.deg)]
+telescope_constraints = [AtNightConstraint(), AltitudeConstraint(10*u.deg, 80*u.deg)]
 
 # now create a scheduler object, in this case we want the scheduler which uses
 # the Greedy method for scheduling
 scheduler = GreedyScheduler(schedule_start, schedule_end, constraints=observer_constraints,
-                            observer=mro, transitioner=transitioner)
+                            observer=apo, transitioner=transitioner)
 
 # run the scheduler on the defined blocks
 schedule = scheduler(blocks)
@@ -89,11 +101,11 @@ schedule = scheduler(blocks)
 # to see the chronological list of the OBs and when they are scheduled for
 print(schedule.scheduled)
 
-# to see the above, but in table form instead (with columns of target, start, end, target_ra, target_dec, score, etc.)
-print(schedule.to_table)
+# to see the above, but as an Astropy Table instead (with columns of target, start, end, target_ra, target_dec, score, etc.)
+print(schedule.to_table())
 
 # plotting when the targets were scheduled and the airmass of the targets.
-plot_schedule_airmass(schedule)
+schedule.plot_airmass()
 
 # to see how much of the time is spent observing
 print(schedule.up_time, schedule.down_time)
