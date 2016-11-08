9f65eba42ffab8904d5ec3be4987480d6a5491d9
Brigitta Sipocz <bsipocz@gmail.com>
2016-11-08 16:47:55 +0000

    Adding exception for time_grid_from_range() when longer than 2 ranges are passed

--- astroplan/tests/test_utils.py
+++ astroplan/tests/test_utils.py
@@ -8,10 +8,14 @@ from astropy.time import Time
 from astropy.tests.helper import remote_data
 from astropy.utils.data import clear_download_cache
 from astropy.utils import iers
+from astropy.tests.helper import pytest
 
-from ..utils import (download_IERS_A, IERS_A_in_cache, get_IERS_A_or_workaround,
-                     BACKUP_Time_get_delta_ut1_utc, stride_array)
+from ..utils import (download_IERS_A, IERS_A_in_cache,
+                     get_IERS_A_or_workaround, BACKUP_Time_get_delta_ut1_utc,
+                     stride_array, time_grid_from_range)
+
 from ..exceptions import OldEarthOrientationDataWarning
+
 
 @remote_data
 def test_iers_download(monkeypatch, recwarn):
@@ -34,14 +38,30 @@ def test_iers_download(monkeypatch, recwarn):
 
     download_IERS_A()
 
-    #now test that it actually works post-IERS A download:
+    # now test that it actually works post-IERS A download:
     nowplusoneyear.ut1
 
 arr10 = np.arange(10)
+
+
 def test_stride_array():
-    stride10by5 = stride_array(arr10,5)
-    assert len(stride10by5)==6
+    stride10by5 = stride_array(arr10, 5)
+    assert len(stride10by5) == 6
+
 
 def test_stride_floats():
-    arr_float = np.asarray(arr10,float)
-    stride10by3 = stride_array(arr_float,3)
+    arr_float = np.asarray(arr10, float)
+    stride10by3 = stride_array(arr_float, 3)
+
+
+def test_time_grid_from_range():
+    times2 = ['2010-01-01 00:00:00', '2010-01-01 01:00:10']
+    times3 = ['2010-01-01 00:00:00', '2010-01-01 01:00:00',
+              '2010-01-01 03:00:00']
+    tgrid = time_grid_from_range(Time(times2))
+    assert len(tgrid) == 3
+    assert np.all(tgrid == Time(['2010-01-01 00:00:00', '2010-01-01 00:30:00',
+                                 '2010-01-01 01:00:00']))
+
+    with pytest.raises(ValueError):
+        time_grid_from_range(Time(times3))
