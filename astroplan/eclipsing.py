239bf829d83cbdb67c37857968798503a942f9b2
Brett Morris <brettmorris21@gmail.com>
2017-09-15 10:14:26 +0200

    Cleaning up tests

--- astroplan/eclipsing.py
+++ astroplan/eclipsing.py
@@ -1,8 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-import astropy.units as u
 import numpy as np
+import astropy.units as u
+from astropy.time import Time
 
 __all__ = ['PeriodicEvent', 'EclipsingSystem']
 
@@ -49,7 +50,7 @@ class PeriodicEvent(object):
             Phase at each ``time``, on range [0, 1)
         """
         return ((time - self.epoch).to(u.day).value %
-                 self.period.to(u.day).value) / self.period.to(u.day).value
+                self.period.to(u.day).value) / self.period.to(u.day).value
 
 
 class EclipsingSystem(PeriodicEvent):
@@ -57,7 +58,7 @@ class EclipsingSystem(PeriodicEvent):
     Define some parameters for an eclipsing system, either an eclipsing binary
     or a transiting exoplanet.
     """
-    def primary_eclipse(self, time):
+    def in_primary_eclipse(self, time):
         """
         Returns `True` when ``time`` is during a primary eclipse.
 
@@ -75,7 +76,7 @@ class EclipsingSystem(PeriodicEvent):
         return ((phases < float(self.duration/self.period)/2) |
                 (phases > 1 - float(self.duration/self.period)/2))
 
-    def secondary_eclipse(self, time, secondary_eclipse_phase=0.5):
+    def in_secondary_eclipse(self, time, secondary_eclipse_phase=0.5):
         """
         Returns `True` when ``time`` is during a secondary eclipse.
 
@@ -94,7 +95,7 @@ class EclipsingSystem(PeriodicEvent):
             ``True`` if ``time`` is during secondary eclipse
         """
         phases = self.phase(time)
-        return ((phases < secondary_eclipse_phase + float(self.duration/self.period)/2) |
+        return ((phases < secondary_eclipse_phase + float(self.duration/self.period)/2) &
                 (phases > secondary_eclipse_phase - float(self.duration/self.period)/2))
 
     def out_of_eclipse(self, time):
@@ -111,9 +112,10 @@ class EclipsingSystem(PeriodicEvent):
         in_eclipse : `~numpy.ndarray` or bool
             ``True`` if ``time`` is not during primary or secondary eclipse
         """
-        return np.logical_not(self.primary_eclipse(time) | self.secondary_eclipse(time))
+        return np.logical_not(np.logical_or(self.in_primary_eclipse(time),
+                                            self.in_secondary_eclipse(time)))
 
-    def next_primary_eclipse(self, time, n_eclipses=1):
+    def next_primary_eclipse_time(self, time, n_eclipses=1):
         """
         Time of the next primary eclipse after ``time``.
 
@@ -132,9 +134,9 @@ class EclipsingSystem(PeriodicEvent):
         """
         eclipse_times = ((1-self.phase(time)) * self.period + time +
                          np.arange(n_eclipses) * self.period)
-        return eclipse_times if len(eclipse_times) > 1 else eclipse_times[0]
+        return eclipse_times
 
-    def next_secondary_eclipse(self, time, n_eclipses=1):
+    def next_secondary_eclipse_time(self, time, n_eclipses=1):
         """
         Time of the next secondary eclipse after ``time``.
 
@@ -158,4 +160,60 @@ class EclipsingSystem(PeriodicEvent):
             next_eclipse_phase = 0.5
         eclipse_times = ((next_eclipse_phase - phase) * self.period + time +
                          np.arange(n_eclipses) * self.period)
-        return eclipse_times if len(eclipse_times) > 1 else eclipse_times[0]
+        return eclipse_times
+
+    def primary_ingress_egress_time(self, time, n_eclipses=1):
+        """
+        Calculate the times of ingress and egress for the next ``n_eclipses``
+        primary eclipses after ``time``
+
+        Parameters
+        ----------
+        time : `~astropy.time.Time`
+            Find the next primary ingress and egress after ``time``
+        n_eclipses : int (optional)
+            Return the times of eclipse for the next ``n_eclipses`` after
+            ``time``. Default is 1.
+
+        Returns
+        -------
+        primary_eclipses : `~astropy.time.Time` of shape (``n_eclipses``, 2)
+            Times of ingress and egress for the next ``n_eclipses`` primary
+            eclipses after ``time``
+        """
+        next_mid_eclipses = self.next_primary_eclipse_time(time, n_eclipses=n_eclipses)
+        next_ingresses = next_mid_eclipses - self.duration/2
+        next_egresses = next_mid_eclipses + self.duration/2
+
+        ing_egr = np.vstack([next_ingresses.utc.jd, next_egresses.utc.jd]).T
+
+        return Time(ing_egr, format='jd', scale='utc')
+
+    def secondary_ingress_egress_time(self, time, n_eclipses=1):
+        """
+        Calculate the times of ingress and egress for the next ``n_eclipses``
+        secondary eclipses after ``time``
+
+        Parameters
+        ----------
+        time : `~astropy.time.Time`
+            Find the next primary ingress and egress after ``time``
+        n_eclipses : int (optional)
+            Return the times of eclipse for the next ``n_eclipses`` after
+            ``time``. Default is 1.
+
+        Returns
+        -------
+        primary_eclipses : `~astropy.time.Time` of shape (``n_eclipses``, 2)
+            Times of ingress and egress for the next ``n_eclipses`` primary
+            eclipses after ``time``
+        """
+        next_mid_eclipses = self.next_secondary_eclipse_time(time, n_eclipses=n_eclipses)
+        next_ingresses = next_mid_eclipses - self.duration/2
+        next_egresses = next_mid_eclipses + self.duration/2
+
+        ing_egr = np.vstack([next_ingresses.utc.jd, next_egresses.utc.jd]).T
+
+        return Time(ing_egr, format='jd', scale='utc')
+
+
--- astroplan/tests/test_eclipsing.py
+++ astroplan/tests/test_eclipsing.py
@@ -4,12 +4,15 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 from astropy.time import Time
 import astropy.units as u
+from numpy.testing import assert_allclose
 
 from ..eclipsing import PeriodicEvent, EclipsingSystem
 
+PRECISION = 0.00001  # days
+
 
 def test_phase():
-    epoch = Time('2016-01-01')
+    epoch = Time('2016-01-01 00:00')
     period = 3*u.day
     duration = 1*u.hour
     pe = PeriodicEvent(epoch=epoch, period=period, duration=duration,
@@ -20,39 +23,75 @@ def test_phase():
 
 
 def test_primary_secondary_eclipse():
-    epoch = Time('2016-01-01')
-    period = 3*u.day
-    duration = 1*u.hour
-    es = EclipsingSystem(epoch=epoch, period=period, duration=duration,
-                       name='test event')
-
-    assert es.next_primary_eclipse(epoch) == Time('2016-01-04 00:00')
-    assert es.next_secondary_eclipse(epoch) == Time('2016-01-02 12:00')
-
-
-def test_out_of_eclipse():
-    epoch = Time('2016-01-01')
+    epoch = Time('2016-01-01 00:00')
     period = 3*u.day
     duration = 1*u.hour
     es = EclipsingSystem(epoch=epoch, period=period, duration=duration,
                          name='test event')
 
-    test_times = Time(['2016-01-02 12:00', '2016-01-03 00:00',
+    ap_primary = es.next_primary_eclipse_time(epoch)
+    ap_secondary = es.next_secondary_eclipse_time(epoch)
+
+    soln_primary = Time(['2016-01-04 00:00'])
+    soln_secondary = Time(['2016-01-02 12:00'])
+
+    # Tolerance of 1 second
+    assert_allclose([ap_primary.jd, ap_secondary.jd],
+                    [soln_primary.jd, soln_secondary.jd], atol=PRECISION)
+
+
+def test_out_of_eclipse():
+    epoch = Time('2016-01-01 00:00')
+    period = 3*u.day
+    duration = 1*u.hour
+    es = EclipsingSystem(epoch=epoch, period=period, duration=duration,
+                         name='test event')
+
+    test_times = Time(['2016-01-01 06:00', '2016-01-02 12:00',
                        '2016-01-04 00:00', '2016-01-05 00:00'])
+
     assert np.all(es.out_of_eclipse(test_times) ==
-                  np.array([False, True, False, True]))
+                  np.array([True, False, False, True]))
 
 
 def test_next_eclipse():
-    epoch = Time('2016-01-01')
+    epoch = Time('2016-01-01 00:00')
     period = 3*u.day
     duration = 1*u.hour
     es = EclipsingSystem(epoch=epoch, period=period, duration=duration,
                          name='test event')
 
     test_time = epoch + period + 0.1*u.min
-    next_primary = es.next_primary_eclipse(test_time)
-    next_secondary = es.next_secondary_eclipse(test_time)
+    ap_next_primary = es.next_primary_eclipse_time(test_time)
+    ap_next_secondary = es.next_secondary_eclipse_time(test_time)
 
-    assert next_primary == Time('2016-01-04')
-    assert next_secondary == Time('2016-01-05 12:00')
+    soln_next_primary = Time(['2016-01-07 00:00'])
+    soln_next_secondary = Time(['2016-01-05 12:00'])
+
+    # Tolerance of 1 second
+    assert_allclose([ap_next_primary.jd, ap_next_secondary.jd],
+                    [soln_next_primary.jd, soln_next_secondary.jd],
+                    atol=PRECISION)
+
+
+def test_primary_ingress():
+    epoch = Time('2016-01-01 00:00')
+    period = 3*u.day
+    duration = 1*u.hour
+    es = EclipsingSystem(epoch=epoch, period=period, duration=duration,
+                         name='test event')
+
+    test_time = epoch + 0.1*u.min
+    ap_next_ing_egr_0 = es.primary_ingress_egress_time(test_time)
+    ap_next_ing_egr_1 = es.primary_ingress_egress_time(test_time, n_eclipses=3)
+
+    soln_next_ing_egr_0 = Time([['2016-01-03 23:30', '2016-01-04 00:30']])
+    soln_next_ing_egr_1 = Time([['2016-01-03 23:30', '2016-01-04 00:30'],
+                                ['2016-01-06 23:30', '2016-01-07 00:30'],
+                                ['2016-01-09 23:30', '2016-01-10 00:30']])
+
+    # Tolerance of 1 second
+    assert_allclose(ap_next_ing_egr_0.jd, soln_next_ing_egr_0.jd,
+                    atol=PRECISION)
+    assert_allclose(ap_next_ing_egr_1.jd, soln_next_ing_egr_1.jd,
+                    atol=PRECISION)
