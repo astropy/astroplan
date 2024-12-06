fd4b30eded1ea4bdbf782bcf0f16ed531b59dc18
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2024-12-06 00:39:57 -1000

    Fix version

--- astroplan/__init__.py
+++ astroplan/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 try:
-    from .version import version as __version__
+    from ._version import version as __version__
 except ImportError:
     __version__ = ''
 
